eef25ebb1135b413297cfa7ed7875c16b2afd371
league <league@imac-14.attlocal.net>
2023-06-20 11:56:51 -0700

    started new moduele

---
+++ src/_00_Intro_To_ArrayLists/runner.java
@@ -0,0 +1,11 @@
+package _00_Intro_To_ArrayLists;
+
+public class runner {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		_02_GuestBook book = new _02_GuestBook();
+		book.run();
+	}
+
+}
