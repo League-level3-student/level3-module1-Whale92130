70d5238f11d1f61bedb659446000df19e4951b6e
league <league@imac.attlocal.net>
2023-06-27 11:55:57 -0700

    finished meeting scheduler and worked on california weather

--- src/_07_Meeting_Scheduler/MeetingScheduler.java
+++ src/_07_Meeting_Scheduler/MeetingScheduler.java
@@ -4,37 +4,59 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 public class MeetingScheduler {
-    /*
-     * Your task is to code a method to find a meeting time for two people
-     * given their schedules.
-     * 
-     * Code the method below so that it returns a Schedule object that contains
-     * all the times during the week that are in BOTH people's schedules. The
-     * Schedule class is included in this package.
-     * 
-     * Example:
-     * person1 availability - Monday at 9, Tuesday at 14, and Friday 10
-     * person2 availability - Tuesday at 14, Friday 8, and Monday at 9
-     * The returned HashMap should contain: Tuesday 14 and Monday 9
-     * 
-     * The returned Schedule object represents the times both people are
-     * available for a meeting.
-     * 
-     * Time availability is always at the top of the hour, so 9:30 is not valid
-     * Time availability always represents 1 hour
-     * Assume both schedules are in the same time zones
-     */
-    public static Schedule getMutualAvailability(Schedule person1, Schedule person2) {
-    	HashMap<String, ArrayList<Integer>> sche1 = person1.getSchedule();
-    	HashMap<String, ArrayList<Integer>> sche2 = person2.getSchedule();
-    	ArrayList<Integer> timesFor1;
-    	Schedule timeThatWorks = null;
-       
-    	for (int i = 0; i < sche1.size();i++) {
-    		timesFor1 = sche1.get("Monday");
-    		
-    	}
-    	
-        return timeThatWorks;
-    }
+	/*
+	 * Your task is to code a method to find a meeting time for two people given
+	 * their schedules.
+	 * 
+	 * Code the method below so that it returns a Schedule object that contains all
+	 * the times during the week that are in BOTH people's schedules. The Schedule
+	 * class is included in this package.
+	 * 
+	 * 
+	 * System.out.println();
+	 * 
+	 * Example: person1 availability - Monday at 9, Tuesday at 14, and Friday 10
+	 * person2 availability - Tuesday at 14, Friday 8, and Monday at 9 The returned
+	 * HashMap should contain: Tuesday 14 and Monday 9
+	 * 
+	 * The returned Schedule object represents the times both people are available
+	 * for a meeting.
+	 * 
+	 * Time availability is always at the top of the hour, so 9:30 is not valid Time
+	 * availability always represents 1 hour Assume both schedules are in the same
+	 * time zones
+	 */
+	public static Schedule getMutualAvailability(Schedule person1, Schedule person2) {
+		HashMap<String, ArrayList<Integer>> sche1 = person1.getSchedule();
+		HashMap<String, ArrayList<Integer>> sche2 = person2.getSchedule();
+		
+		Schedule timeThatWorks = new Schedule();
+
+		String[] days = new String[7];
+		days[0] = "Monday";
+		days[1] = "Tuesday";
+		days[2] = "Wednesday";
+		days[3] = "Thursday";
+		days[4] = "Friday";
+		days[5] = "Saturday";
+		days[6] = "Sunday";
+
+		for (int q = 0; q < days.length; q++) {
+			ArrayList<Integer> timesFor1 = new ArrayList<Integer>();
+			ArrayList<Integer> timesFor2 = new ArrayList<Integer>();
+			timesFor1.addAll(sche1.get(days[q]));
+			timesFor2.addAll(sche2.get(days[q]));
+		
+			for (int i = 0; i < timesFor1.size(); i++) {
+				for (int o = 0; o < timesFor2.size(); o++) {
+					if (timesFor1.get(i) == timesFor2.get(o)) {
+						timeThatWorks.addAvailability(days[q], timesFor1.get(i));
+					}
+				}
+			}
+			
+		}
+
+		return timeThatWorks;
+	}
 }
--- src/_08_California_Weather/CaliforniaWeather.java
+++ src/_08_California_Weather/CaliforniaWeather.java
@@ -1,6 +1,13 @@
 package _08_California_Weather;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.HashMap;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
 
 /*
  * OBJECTIVE:
@@ -27,19 +34,78 @@ import java.util.HashMap;
  * temperature, you can get a free API key at: https://openweathermap.org/api
  */
 
-public class CaliforniaWeather {
-    
-    void start() {
-        HashMap<String, WeatherData> weatherData = Utilities.getWeatherData();
-        
-        // All city keys have the first letter capitalized of each word
-        String cityName = Utilities.capitalizeWords( "National City" );
-        WeatherData datum = weatherData.get(cityName);
-        
-        if( datum == null ) {
-            System.out.println("Unable to find weather data for: " + cityName);
-        } else {
-            System.out.println(cityName + " is " + datum.weatherSummary + " with a temperature of " + datum.temperatureF + " F");
-        }
-    }
+public class CaliforniaWeather implements ActionListener {
+
+	JFrame frame = new JFrame();
+	JPanel panel = new JPanel();
+	JButton button1 = new JButton();
+	JButton button2 = new JButton();
+	JButton button3 = new JButton();
+	JButton button4 = new JButton();
+	HashMap<String, WeatherData> weatherData = Utilities.getWeatherData();
+
+	void start() {
+
+		frame.add(panel);
+		panel.add(button1);
+		button1.addActionListener(this);
+		panel.add(button2);
+		button2.addActionListener(this);
+		panel.add(button3);
+		button3.addActionListener(this);
+		panel.add(button4);
+		button4.addActionListener(this);
+
+		button1.setText("Search by City");
+		button2.setText("Search by Current Conditions");
+		button3.setText("Search by Max Temperature");
+		button4.setText("Search by Minimum Temperature");
+
+		frame.pack();
+		frame.setDefaultCloseOperation(frame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+
+		// All city keys have the first letter capitalized of each word
+
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+		// button1
+		if (arg0.getSource().equals(button1)) {
+			String cityName = JOptionPane.showInputDialog("Type City Name");
+			WeatherData datum = weatherData.get(cityName);
+
+			if (datum == null) {
+				JOptionPane.showMessageDialog(null, "Unable to find weather data for: " + cityName);
+			} else {
+				JOptionPane.showMessageDialog(null, cityName + " is " + datum.weatherSummary + " with a temperature of "
+						+ datum.temperatureF + " F");
+			}
+		}
+
+		// button 2
+		if (arg0.getSource().equals(button2)) {
+			String conditions = JOptionPane.showInputDialog("Type Current City Conditions");
+			WeatherData datum = weatherData.get(conditions);
+			System.out.println(datum);
+			if (datum == null) {
+				JOptionPane.showMessageDialog(null, "Unable to find city data for: " + conditions);
+			} else {
+				JOptionPane.showMessageDialog(null, datum);
+			}
+		}
+
+		// button 3
+		if (arg0.getSource().equals(button3)) {
+
+		}
+
+		// button 4
+		if (arg0.getSource().equals(button4)) {
+
+		}
+
+	}
 }
